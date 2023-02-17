069bb979a593579591c02123ccdb8ff98207f10a
Furkan Ünal <furkanunal89@gmail.com>
2023-02-17 22:43:32 +0300

    initial commit

---
+++ backend/backend.py
@@ -0,0 +1,19 @@
+"""
+This script will handle creating, editing and deleting tasks
+"""
+
+
+def create_todo():
+    print("task create")
+
+
+def delete_todo():
+    print("task delete")
+
+
+def create_task():
+    print("subtask create")
+
+
+def delete_task():
+    print("subtask edit")
